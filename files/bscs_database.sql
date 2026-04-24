0c74edfd72e8aa04398ab4ee64212eb8dfcbe7f7
Jerwin Cruz <cruz.jerwin15@gmail.com>
2026-04-24 12:17:30 +0800

    Add new model, controller and routes

--- files/bscs_database.sql
+++ files/bscs_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 23, 2026 at 04:45 AM
+-- Generation Time: Apr 24, 2026 at 06:16 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -18,8 +18,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_bscs`
+-- Database: `bscs_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `id` int(10) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `course` varchar(6) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_student`
+--
+
+INSERT INTO `tbl_student` (`id`, `firstname`, `lastname`, `course`, `gender`, `status`) VALUES
+(1, 'Tony', 'Stark', 'BSCS', 'M', 'ACTIVE'),
+(2, 'Steve', 'Rogers', 'BSIT', 'M', 'ACTIVE'),
+(3, 'Jerwin', 'Cruz', 'BSCS', 'M', 'ACTIVE'),
+(4, 'Bruce', 'Banner', 'BSE', 'M', 'ACTIVE');
 
 -- --------------------------------------------------------
 
@@ -40,11 +65,22 @@ CREATE TABLE `tbl_users` (
 
 INSERT INTO `tbl_users` (`id`, `firstname`, `lastname`, `status`) VALUES
 (1, 'Jerwin', 'Cruz', 'ACTIVE'),
-(2, 'Peter', 'Parker', 'ACTIVE');
+(2, 'Peter', 'Parker', 'ACTIVE'),
+(3, 'Steve', 'Rogers', 'ACTIVE'),
+(4, 'Tony', 'Starks', 'ACTIVE'),
+(5, 'Bruce', 'Banner', 'ACTIVE'),
+(6, 'Thor', 'Odinson', 'ACTIVE'),
+(10, 'Steven', 'Strange', 'ACTIVE');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_users`
@@ -57,10 +93,16 @@ ALTER TABLE `tbl_users`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
